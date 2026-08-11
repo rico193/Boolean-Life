34ea6b50fa758b43457f3cefbae5775e83fc3357
rico193 <muhammadzainimran193@gmail.com>
2026-08-11 18:35:50 +0500

    did tiny bit of work ig

--- boolean life.cpp
+++ boolean life.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 #include <ctime> //for random number generation
 using namespace std;
- // This project is a copy/fork of the game known as "Bit Life" on Google Playstore. The original game was created by Candywriter, LLC. This project is not affiliated with Candywriter, LLC in any way. This project is a personal project and is not intended for commercial use. The purpose of this project is to learn C++ programming and to create a simple text-based life simulation game. As a personal project, it is not intended for commercial use and is solely for educational purposes. The game simulates various life events and decisions, allowing players to experience different scenarios and outcomes based on their choices.
 
- //Work on the project will begin shortly. I will just be stating the functions in comments as of now.
- //Age Variable starts at 0, random events will occur as the player ages, based on age category. if age is in a specific range, certain events will be triggered according to it. I will be adding 15 events per category as I do not want the code to be too complex. Stress will lower health, and if health is low enough, it will eventually lead to death.
-
- //player will be able to enter his first name, and last name. Parent's names will be randomly generated. Player will be able to choose his gender, and the game will have some set rules I will define later.
-
- //I have no idea how many functions there are gonna be lol
  int main()
 {
     cout << R"(
@@ -21,5 +14,29 @@ using namespace std;
                                     (Developed by Rico)   
     )" << endl;
     system("pause");
+
+    bool gameRunning = true;
+    int age=0;
+    int choice=-1;
+    string FirstName, LastName;
+
+    cout<<endl;
+    while (gameRunning==true)
+    {
+        cout<<"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+        cout<<"Menu"<<endl;
+        cout<<"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+        cout<<endl;
+        cout<<"Enter your choice: ";
+        cin>>choice;
+        cout<<endl;
+        if (choice==1)
+        {
+            age++;
+        }
+        //Reset value of choice to -1 to avoid infinite loop
+        choice=-1;
+    }
+
 
 }

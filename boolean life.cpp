e96f4307572085bc1b43c25a7b07916cc163f519
rico193 <muhammadzainimran193@gmail.com>
2026-08-21 23:27:12 +0500

    Updated Interface, laid foundation to build on

--- boolean life.cpp
+++ boolean life.cpp
@@ -15,38 +15,69 @@ using namespace std;
                                     (Developed by Rico)   
     )" << endl;
     system("pause");
-
+    
+    int gender_determiner=rand()%2;
+    string gender="To be Determined";
+    if (gender_determiner==0)
+    {
+        gender="Male";
+    }
+    else
+    {
+        gender="Female";
+    }
+    
     bool gameRunning = true;
     int age=0;
     int choice=-1;
     string FirstName,  LastName;
+    cout<<endl;
+    cout<<"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+    cout<<"Gender: "<<gender<<endl;
+    cout<<"Country: Boolean Land\n";    
+    cout<<"I was born a "<<gender<<" in Boolean Land\n";
+    cout<<"Enter your first name: ";
+    cin>>FirstName;
+    cout<<"Enter your last name: ";
+    cin>>LastName;
+    cout<<"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+    cout<<endl;
 
     while (gameRunning==true)
     {
         cout<<"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
         cout<<"Options:"<<endl;
-        cout<<"Age+ <\n";
-        cout<<"Manage Relationships\n";
-        cout<<"Manage Finances\n";
-        cout<<"Manage Health\n";
-        cout<<"Manage Career\n";
-        cout<<"Manage Education\n";
-        cout<<"Manage Hobbies\n";
+        cout<<"1. Age+ \n";
+        cout<<"2. Manage Relationships\n";
+        cout<<"3. Manage Finances\n";
+        cout<<"4. Manage Health\n";
+        cout<<"5. Manage Career\n";
+        cout<<"6. Manage Education\n";
+        cout<<"7. Manage Hobbies\n";
+        cout<<"8. Misc\n"; 
+        cout<<"8. End Game\n";
         cout<<"+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
         cout<<endl;
         cout<<"Enter your choice: ";
         cin>>choice;
         cout<<endl;
         if (choice==1)
-        {
-            age++; //git add "boolean life.cpp" git commit -m "Added age increment feature" git push origin main     
+        { 
+            age++; //git add "boolean life.cpp" git commit -m "Added age increment feature" git push origin main 
+            cout<<endl; 
+            cout<<"You are now "<<age<<" years old."<<endl<<endl;    
         }
+       
+
+        if (choice==8)  
+        {
+            return 0;
+        }
+
         //Reset value of choice to -1 to avoid infinite loop
         choice=-1;            
     }
-
-
-}   
+} 
 
 
 
